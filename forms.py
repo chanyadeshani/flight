b78ba836a4912df98c7ea31de6db8845e403e18c
chanyadeshani <chanya.deshani@gmail.com>
2024-03-25 23:59:21 +0000

    customer done

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+                     RadioField)
+from wtforms.validators import InputRequired, Length
+
+
+class CustomerForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired(),
+                                                       Length(min=10, max=100)])
+    second_name = StringField('Second Name', validators=[InputRequired(),
+                                                         Length(min=10, max=100)])
+    last_name = StringField('Last Name', validators=[InputRequired(),
+                                                     Length(min=10, max=100)])
+    phone_number = StringField('Phone Number', validators=[InputRequired(),
+                                                           Length(min=11, max=11)])
+    email_address = StringField('Email Address', validators=[InputRequired(),
+                                                             Length(min=6, max=35)])
+    address = StringField('Address', validators=[InputRequired(),
+                                                 Length(min=6, max=100)])
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import database
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+                     RadioField)
+from wtforms.validators import InputRequired, Length
+
+import forms
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/')
 def index():
@@ -17,16 +23,40 @@ def submit_order():
     destination = data.get('destination')
     stops = data.get('stops')
     seats = data.get('seats')
-    from_date= data.get('date_from')
+    from_date = data.get('date_from')
     to_date = data.get('date_to')
-    available_flights = [0,0,0,0,0,0,0]
+    available_flights = [0, 0, 0, 0, 0, 0, 0]
     available_flights2 = ''
     if flightType.lower() == "one-way" or flightType.lower() == "round-trip":
-        flight = [origin, destination,from_date]
-        available_flights = database.get_flights('flight.db', flight[0],flight[1],flight[2])
+        flight = [origin, destination, from_date]
+        available_flights = database.get_flights('flight.db', flight[0], flight[1], flight[2])
         print("test", len(available_flights))
 
     return available_flights
+
+
+@app.route('/confirm')
+def customers():
+    return "<H1> Successful</H1>"
+
+courses_list = [{
+    'title': 'Python 101',
+    'description': 'Learn Python basics',
+    'price': 34,
+    'available': True,
+    'level': 'Beginner'
+    }]
+
+
+@app.route('/customer', methods=('GET', 'POST'))
+def customer():
+    form = forms.CustomerForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        # Process form data if it's a POST request and form is valid
+        # Example: Save form data to the database
+        return 'Form submitted successfully!'
+    return render_template('Customer_details.html', form=form)
+
 
 
 if __name__ == '__main__':
